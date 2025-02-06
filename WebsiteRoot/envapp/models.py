3355907f0f630a7431a7fa003a69c39c4fb73bee
Trasgo <roeltaylor@protonmail.com>
2025-02-06 22:12:56 +0000

    Add extended User model and Video model
    This adds several models:
    
    First is the UserTable, which extends the built in User model with two new fields: role and points. Role is for storing the user's role as a string, and points is for storing the number of points the user has accrued
    
    The Videos model has three fields: videoTitle, videoLink, videoPoints. These are for storing the title, link to the video and the number points given for watching respectively. There is also a videoThumbnailURL field to store the link to the video thumbnail (intended to be an image)
    
    Another model, VideoWatchers, is used to identify which users have already watched what video. This way, we can make sure that points arent repeatedly awareded. This uses the id field from both the UserTable and Videos models as foreign keys, and adds a timestamp when the video was watched.

--- WebsiteRoot/envapp/models.py
+++ WebsiteRoot/envapp/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-
-class Test(models.Model):
-    testfield = models.CharField(max_length=200)
-    testdate = models.DateTimeField("date published")
+ 
+class UserTable(AbstractUser):
+    # This extends the included User model 
+    role = models.CharField(max_length=30) 
+    points = models.IntegerField(default=0)
+    
+class Videos(models.Model):
+    #This stores the videos for the eventual video watching task
+    videoTitle = models.CharField(max_length=200) # For video title
+    videoThumbnailURL = models.CharField(max_length=200) # For link to an image file to display as a thumbnail
+    videoLink = models.CharField(max_length=200) # Actual link to the video
+    videoPoints = models.IntegerField(default=0, null=False) # Number of points given for watching
+    
+    def __str__(self):
+        return self.videoTitle
+        
+class VideoWatchers(models.Model):
+    userID = models.ForeignKey(UserTable, on_delete=models.CASCADE) # Stores the user ID of the user who has watched the video
+    videoID = models.ForeignKey(Videos, on_delete=models.CASCADE) # Stores the video ID of the video that has been watched
+    watchTime = models.DateTimeField(auto_now_add=True) # Automatically adds the date the video has been watched/clicked
