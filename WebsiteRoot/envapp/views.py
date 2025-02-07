e9db16b0aae4c5d482a9dde3d0009fc3786b27e7
Lorenzo <lc866@exeter.ac.uk>
2025-02-07 01:23:03 +0000

    envapp/templates/envapp directory created for html files

--- WebsiteRoot/envapp/views.py
+++ WebsiteRoot/envapp/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def index(request):
     return HttpResponse("This is the index page of our app")
+
+def register(request):
+    if request.method == 'POST':  # Handle form submission
+        form = UserCreationForm(request.POST)
+        if form.is_valid():  # If the form is valid
+            form.save()  # Save the new user
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')  # Redirect to login page after registration
+    else:
+        form = UserCreationForm()  # Display an empty form for GET requests
+    return render(request, 'envapp/register.html', {'form': form})
