22e94a4891b5175acb5d460c95d73e8bd8a872d3
Nelbut <96624456+Nelbut@users.noreply.github.com>
2025-02-08 16:31:46 +0000

    Update urls.py

--- WebsiteRoot/envapp/urls.py
+++ WebsiteRoot/envapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('register/', views.register, name='register'),  # Registration page
     path('gamekeeper/', views.gamekeeper, name='gamekeeper'),
+    path ('Admin/', views.admin_login, name = 'Admin') # Admin login
 ]
