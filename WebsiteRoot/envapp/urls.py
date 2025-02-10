79f151394a062d4b2ed30342db848b6070016ec9
HecateII <111183999+HecateMKY@users.noreply.github.com>
2025-02-10 01:58:25 +0000

    Add files via upload
    1. Separated challenges
    2. Added submission control
    
    Changes on models.py, urls.py, views.py and templates

--- WebsiteRoot/envapp/urls.py
+++ WebsiteRoot/envapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views import challenge_list, create_challenge, edit_challenge, delete_challenge
 from . import views
 
 urlpatterns = [
@@ -9,4 +9,9 @@ urlpatterns = [
     path('gamekeeper/', views.gamekeeper, name='gamekeeper'),
     path('Admin/', views.admin_login, name = 'Admin'), # Admin login
     path('student_dashboard/', views.student_dashboard, name='student_dashboard'),  # Add this line
+    path('review-submissions/', review_submissions, name='review_submissions'), #review_submissions
+    path('challenges/', challenge_list, name='challenge_list'),
+    path('challenges/create/', create_challenge, name='create_challenge'),
+    path('challenges/edit/<int:challenge_id>/', edit_challenge, name='edit_challenge'),
+    path('challenges/delete/<int:challenge_id>/', delete_challenge, name='delete_challenge'),
 ]
