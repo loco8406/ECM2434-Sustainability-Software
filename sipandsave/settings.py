7197b53121abb5c521e8e8bc342d8a98a717e5d1
Rahul Pawan Somani <rps210@exeter.ac.uk>
2025-03-22 13:34:27 +0000

    fixing settings.py

--- sipandsave/settings.py
+++ sipandsave/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-c9_v7xnynx_j+nlnz451sgbe!5^5p&mfhg1$m7umu(yp=_j4d_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
